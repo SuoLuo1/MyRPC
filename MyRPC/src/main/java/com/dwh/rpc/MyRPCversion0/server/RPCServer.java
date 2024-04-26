ea7649ad3b2b61f25db9b80d204d2a74f08b3b0f
dwh <756173757@qq.com>
2024-04-26 21:25:44 +0800

    version1

--- MyRPC/src/main/java/com/dwh/rpc/MyRPCversion0/server/RPCServer.java
+++ MyRPC/src/main/java/com/dwh/rpc/MyRPCversion0/server/RPCServer.java
@@ -1,10 +1,14 @@
 package com.dwh.rpc.MyRPCversion0.server;
 
+import com.dwh.rpc.MyRPCversion0.common.RPCRequest;
+import com.dwh.rpc.MyRPCversion0.common.RPCResponse;
 import com.dwh.rpc.MyRPCversion0.common.User;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -29,13 +33,16 @@ public class RPCServer {
                     try {
                         ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
                         ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-                        // 读取客户端传过来的id
-                        Integer id = ois.readInt();
-                        User userByUserId = userService.getUserByUserId(id);
-                        // 写入User对象给客户端
-                        oos.writeObject(userByUserId);
+                        // 读取客户端传过来的request
+                        RPCRequest request = (RPCRequest) ois.readObject();
+                        // 反射调用对应方法
+                        Method method = userService.getClass().getMethod(request.getMethodName(), request.getParamsTypes());
+                        Object invoke = method.invoke(userService, request.getParams());
+                        // 封装，写入response对象
+                        oos.writeObject(RPCResponse.success(invoke));
                         oos.flush();
-                    } catch (IOException e){
+                    } catch (IOException | ClassNotFoundException | NoSuchMethodException | IllegalAccessException |
+                             InvocationTargetException e){
                         e.printStackTrace();
                         System.out.println("从IO中读取数据错误");
                     }
--- MyRPC/src/main/java/com/dwh/rpc/MyRPCversion0/service/UserService.java
+++ MyRPC/src/main/java/com/dwh/rpc/MyRPCversion0/service/UserService.java
@@ -10,5 +10,8 @@ import com.dwh.rpc.MyRPCversion0.common.User;
  *@create 2024/4/23 10:41
  */
 public interface UserService {
+    // 客户端通过这个接口调用服务端的实现类
     User getUserByUserId(Integer id);
+    // 给这个服务增加一个功能
+    Integer insertUserId(User user);
 }
